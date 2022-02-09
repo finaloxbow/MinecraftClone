534d066629d1f2e10c1b39ccc8c4e521c0a401a9
finaloxbow <amaanm03@gmail.com>
2022-02-09 18:26:56 -0500

    Added comments for the rendering process.

--- render.cpp
+++ render.cpp
@@ -60,10 +60,30 @@ int main()
         0.5f, -0.5f
     };
 
+    //generating buffers on GPU from vertex data
     unsigned int buffer;
     glGenBuffers(1, &buffer);
+
+    //binding the buffer for use
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //specifying the type, size, actual data, and mode of the vertex data
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    /*
+    - specifying how the vertex data is structured
+    - first param is "index" of the first vertex position attrib. since this is the first one, we use 1
+    - second param is the size of the vertex attribute. for us, it is 2 floats, so use 2.
+    - third param is the type of the vertex data
+    - fourth param is whether to normalize the given data to between -1 and 1. Since we already start with normalized data, it is false
+    - fifth param is the distance from the start of the first vertex attribute to the second one. Since the size of each vertex attribute (position
+    in this case) is 2 floats, the distance from the start of the first attribute to the start of the second is 2 floats
+    - last param is the offset of the first vertex attribute from the beginning of the data
+
+    */
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // render loop
     // -----------
@@ -77,6 +97,8 @@ int main()
         // ------
         glClearColor(0.96f, 0.34f, 0.34f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
