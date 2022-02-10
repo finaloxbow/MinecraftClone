8c9256a031c4377e3d2cd422cc0233e872400e25
finaloxbow <amaanm03@gmail.com>
2022-02-09 19:02:00 -0500

    Added complete code to render triangles and added comments.

--- render.cpp
+++ render.cpp
@@ -5,11 +5,12 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+static unsigned int CompileShader(unsigned int type, const std::string& source);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-static bool isFullscreen = false;
 
 int main()
 {
@@ -61,8 +62,12 @@ int main()
     };
 
     //generating buffers on GPU from vertex data
-    unsigned int buffer;
+    //TODO: find out why VAO required here for objects to render
+    //----------------------------------------------------------
+    unsigned int buffer, VAO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &buffer);
+    glBindVertexArray(VAO);
 
     //binding the buffer for use
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -83,7 +88,33 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //shaders written in GLSL to run on the GPU
+
+    //takes in vec4 as input because of requirements
+    //opengl automatically casts our vertices to vec4s
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    //fragments output the final color of the pixel to the screen
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     // render loop
     // -----------
@@ -95,9 +126,9 @@ int main()
 
         // render
         // ------
-        glClearColor(0.96f, 0.34f, 0.34f, 1.0f);
+        glClearColor(0.46f, 0.89f, 0.51f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        glUseProgram(shader);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
@@ -130,4 +161,30 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    return id;
 }
