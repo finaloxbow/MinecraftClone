65249ea305847c46b1fac22e9e224fb5b4e3d45d
amaan <amaanm03@gmail.com>
2022-07-26 00:56:36 -0400

    Added code for block placing.

--- src/Chunk/ChunkManager.cpp
+++ src/Chunk/ChunkManager.cpp
@@ -11,7 +11,8 @@ ChunkManager::ChunkManager(Camera* camera, MousePicker* picker, GLFWwindow* wind
 
 static long maxFloats = 0;
 static long maxRendered = 0;
-static int counter = 0;
+static double lastLeftClickTime = 0;
+static double lastRightClickTime = 0;
 
 void ChunkManager::Render_Chunks()
 {
@@ -83,6 +84,8 @@ void ChunkManager::Update_Loaded_Chunks()
 
 void ChunkManager::deleteBlockAtRay()
 {
+	bool endLoop = false;
+
 	for (float i = 0.0f; i <= 8.0f; i += 1) {
 		glm::vec3 mouseRay = picker->calcMouseRay(i);
 
@@ -114,8 +117,6 @@ void ChunkManager::deleteBlockAtRay()
 			? chunkList.find({ static_cast<int>(chunkCoords.x),static_cast<int>(chunkCoords.y) })->second
 			: nullptr;
 
-		bool endLoop = false;
-
 		if (contextChunk != nullptr) {
 			//iterate over entire grid and do AABB collision testing
 			for (int x = 0; x < CHUNK_SIZE; x++) {
@@ -146,18 +147,108 @@ void ChunkManager::deleteBlockAtRay()
 					break;
 			}
 		}
+		if (endLoop)
+			break;
 	}
 }
 
 void ChunkManager::addBlockAtRay()
 {
+	bool endLoop = false;
+	bool activeBlockReached = false;
+	glm::vec3 previousEmptyBlock = glm::vec3(0.0f, 0.0f, 0.0f);
 
+	for (float i = 0.0f; i <= 8.0f; i++) {
+		//find first active block
+		//if active block and i >= 1.0f
+		//place block at previous empty position
+		glm::vec3 mouseRay = picker->calcMouseRay(i);
+
+		//find chunk coords (ez)
+		glm::vec2 chunkCoords(0.0f, 0.0f);
+
+		//x coord
+		if (mouseRay.x >= 0)
+			chunkCoords.x = static_cast<int>(mouseRay.x)
+			- (static_cast<int>(mouseRay.x) % CHUNK_SIZE);
+		else {
+			chunkCoords.x = static_cast<int>(mouseRay.x - 1);
+			while (static_cast<int>(chunkCoords.x) % CHUNK_SIZE != 0)
+				chunkCoords.x -= 1;
+		}
+
+		//z coord
+		if (mouseRay.z >= 0)
+			chunkCoords.y = static_cast<int>(mouseRay.z)
+			- (static_cast<int>(mouseRay.z) % CHUNK_SIZE);
+		else {
+			chunkCoords.y = static_cast<int>(mouseRay.z - 1);
+			while (static_cast<int>(chunkCoords.y) % CHUNK_SIZE != 0)
+				chunkCoords.y -= 1;
+		}
+
+		Chunk* contextChunk = chunkList.find({ static_cast<int>(chunkCoords.x),
+			static_cast<int>(chunkCoords.y) }) != chunkList.end()
+			? chunkList.find({ static_cast<int>(chunkCoords.x),static_cast<int>(chunkCoords.y) })->second
+			: nullptr;
+
+		if (contextChunk != nullptr) {
+			//iterate over entire grid and do AABB collision testing
+			for (int x = 0; x < CHUNK_SIZE; x++) {
+				for (int y = 0; y < CHUNK_HEIGHT; y++) {
+					for (int z = 0; z < CHUNK_SIZE; z++) {
+						//COLLISION TESTING: 
+						//m.x inbetween x + chunk.x and x + 1 + chunk.x
+						//m.y inbetween y + chunk.y and y + 1
+						//m.z inbetween z and z + 1
+						if (mouseRay.x >= x + chunkCoords.x
+							&& mouseRay.x <= x + 1 + chunkCoords.x
+							&& mouseRay.y >= y
+							&& mouseRay.y <= y + 1
+							&& mouseRay.z >= z + chunkCoords.y
+							&& mouseRay.z <= z + 1 + chunkCoords.y) {
+							if (contextChunk->isActive(x, y, z) && i >= 1.0f) {
+								//place block at last empty position
+								contextChunk->updateBlock(previousEmptyBlock.x, previousEmptyBlock.y, previousEmptyBlock.z, true);
+								contextChunk->setData();
+								endLoop = true;
+								break;
+							}
+							else if (contextChunk->isActive(x, y, z)) {
+								//break out of loop
+								break;
+							}
+							else {
+								//update lastEmptyBlockVector
+								previousEmptyBlock.x = x;
+								previousEmptyBlock.y = y;
+								previousEmptyBlock.z = z;
+							}
+
+						}
+					}
+					if (endLoop)
+						break;
+				}
+				if (endLoop)
+					break;
+			}
+		}
+		if (endLoop)
+			break;
+	}
 }
 
 void ChunkManager::procBlockInput()
 {
-	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && glm::abs(lastLeftClickTime - glfwGetTime()) >= 0.25) {
+		lastLeftClickTime = glfwGetTime();
 		deleteBlockAtRay();
 		printf("LEFT CLICK\n");
+	}
+	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS && glm::abs(lastRightClickTime - glfwGetTime()) >= 0.25) {
+		lastRightClickTime = glfwGetTime();
+		addBlockAtRay();
+		printf("RIGHT CLICK\n");
 	}
 }
