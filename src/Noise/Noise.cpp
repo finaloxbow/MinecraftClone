ab0b5c0445404ce35e48cca428391679ea53d243
finaloxbow <amaanm03@gmail.com>
2022-08-02 20:10:09 -0400

    Added more interesting world generation.

--- src/Noise/Noise.cpp
+++ src/Noise/Noise.cpp
@@ -3,7 +3,10 @@
 Noise::Noise()
 {
 	noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Perlin);
-	noise.SetFrequency(0.04);
+	noise.SetFrequency(0.005);
+	noise.SetFractalType(FastNoiseLite::FractalType::FractalType_PingPong);
+	noise.SetFractalOctaves(5);
+	noise.SetFractalGain(0.4);
 }
 
 float Noise::getNoise(int x, int z)
